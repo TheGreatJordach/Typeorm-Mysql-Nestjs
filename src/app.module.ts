ea879b2c467017972669de85589de1b7c64efdda
Jordach Makaya H.M <4thinkai@gmail.com>
2024-08-11 22:43:38 +0100

    connection to the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { CommonModule } from './common/common.module';
+import { MysqlDatabaseModule } from './mysql-database/mysql-database.module';
 
 @Module({
-  imports: [UsersModule, CommonModule],
+  imports: [UsersModule, CommonModule, MysqlDatabaseModule],
 })
 export class AppModule {}
---
+++ src/mysql-database/mysql-database.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3309,
+      username: 'root',
+      password: 'Typscript@@2026',
+      database: 'Login-Users',
+      autoLoadEntities: true,
+      logging: false,
+    }),
+  ],
+})
+export class MysqlDatabaseModule {}
