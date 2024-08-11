c7d64e909a0f418f88d127966e0d5ac41dc2010d
Jordach Makaya H.M <4thinkai@gmail.com>
2024-08-11 22:18:07 +0100

    Appliying validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,7 +1,14 @@
+import { IsEmail, IsPhoneNumber, IsString, Length } from 'class-validator';
+
 export class CreateUserDto {
+  @Length(2, 8)
   readonly firstName: string;
+  @Length(2, 8)
   readonly lastName: string;
+  @IsPhoneNumber('MA')
   readonly phone: string;
+  @IsEmail()
   readonly email: string;
+  @IsString()
   readonly password: string;
 }
