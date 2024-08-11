cdfab9e3e3cd1b2eadb0cf190b38c6a6e8f73851
Jordach Makaya H.M <4thinkai@gmail.com>
2024-08-11 23:14:07 +0100

    Mapping user entity to database table

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,8 +1,21 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { RegistryDates } from 'common/endedded/registry-date.embedded';
+
+@Entity('users')
 export class User {
+  @PrimaryGeneratedColumn()
   id: number;
+  @Column()
   firstName: string;
+  @Column()
   lastName: string;
+  @Column({ unique: true })
   phone: string;
+  @Column({ unique: true })
   email: string;
+  @Column()
   password: string;
+
+  @Column(() => RegistryDates, { prefix: false })
+  registryDates: RegistryDates;
 }
